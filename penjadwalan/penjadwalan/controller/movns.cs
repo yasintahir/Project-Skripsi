bc9eca0f6ac0cf1a421507f10df49c8111c5fdc2
Ricky Azhari Pratama <coret.team@gmail.com>
2015-12-05 16:59:03 +0700

    fitness, insertion
    intensification macet

--- penjadwalan/penjadwalan/controller/movns.cs
+++ penjadwalan/penjadwalan/controller/movns.cs
@@ -17,7 +17,6 @@ namespace penjadwalan.controller
         private Random rr = new Random();
         public solusi proses_movns(solusi Solusi, GLobalSolusi global)
         {
-            
             return Solusi;
         }
 
@@ -32,6 +31,8 @@ namespace penjadwalan.controller
 
             int x = rg.Next(0, global.Solusi.Count-1);
             int y = rg.Next(0, global.Solusi.Count-1);
+            int xlimit =  limit(x);
+            int ylimit = limit(y);
             while (y == x)
             {
                 y = rg.Next(0, global.Solusi.Count-1);
@@ -48,18 +49,35 @@ namespace penjadwalan.controller
                 global.Solusi[x].Mengajar[i].StartMengajar = jam;
                 jam = jam + global.Solusi[x].Mengajar[i].Sks;
                 global.Solusi[x].Mengajar[i].EndMengajar = jam - 1;
+                xlimit -= global.Solusi[x].Mengajar[i].Sks;
             }
-
+            global.Solusi[x].Limit = xlimit;
             jam = atur_jam_mengajar(y);
             for (int i = 0; i < global.Solusi[y].Mengajar.Count; i++)
             {
                 global.Solusi[y].Mengajar[i].StartMengajar = jam;
                 jam = jam + global.Solusi[y].Mengajar[i].Sks;
                 global.Solusi[y].Mengajar[i].EndMengajar = jam - 1;
+                ylimit -= global.Solusi[y].Mengajar[i].Sks;
             }
+            global.Solusi[y].Limit = ylimit;
                 return global;
         }
 
+        public int limit(int x)
+        {
+            if (x == 0)
+            {
+                return 9;
+            }else if(x == 4){
+                return 8;
+            }
+            else
+            {
+                return 10;
+            }
+            
+        }
 
         public static int atur_jam_mengajar(int x)
         {
@@ -77,6 +95,107 @@ namespace penjadwalan.controller
                 atur_jam = 1;
             }
             return atur_jam;
+        }
+
+        public int local_pinalti(solusi jadwal)
+        {
+            int pinalti = 0;
+
+            for (int i = 0; i < jadwal.Solusi.Count; i++)
+            {
+                //cek limit hari yang minus
+                if (jadwal.Solusi[i].Limit < 0)
+                {
+                    pinalti++;
+                }
+                //cek limit yang kurang dari 5
+                if (jadwal.Solusi[i].Limit > 3)
+                {
+                    pinalti++;
+                }
+
+                // cek hari mata pelajaran cuma ada satu
+                if (jadwal.Solusi[i].Mengajar.Count == 1)
+                {
+                    pinalti++;
+                }
+                // cek hari mata pelajaran lebih dari 4
+                if (jadwal.Solusi[i].Mengajar.Count > 4)
+                {
+                    pinalti++;
+                }
+            }
+
+                return pinalti;
+        }
+
+        public int global_pinalti(GLobalSolusi GLobalJadwal, int pos)
+        {
+            int pinalti = 0;
+            // cek guru ngajar bentrok dengan kelas sebelumnya
+            for (int i = 0; i < pos; i++)
+            {
+                for (int j = 0; j < GLobalJadwal.GlobalSolusi[i].Solusi.Count; j++)
+                {
+                    for (int k = 0; k < GLobalJadwal.GlobalSolusi[pos].Solusi[j].Mengajar.Count; k++)
+                    {
+                        if (GLobalJadwal.GlobalSolusi[i].Solusi[j].Mengajar.Count - 1 > k)
+                        {
+                            if (GLobalJadwal.GlobalSolusi[i].Solusi[j].Mengajar[k].Guru == GLobalJadwal.GlobalSolusi[pos].Solusi[j].Mengajar[k].Guru)
+                            {
+                                pinalti++;
+                            }
+                        }
+                    }
+                }
+            }
+
+            //cek rentang guru ngajar terlalu dempet ex : kelas-a mp x jam 1-3, kelas-b mp y 4-6
+            for (int i = 0; i < GLobalJadwal.GlobalSolusi[pos].Solusi.Count; i++)
+            {
+                for (int j = 0; j < GLobalJadwal.GlobalSolusi[pos].Solusi[i].Mengajar.Count; j++)
+                {
+                    for (int k = 0; k < pos; k++)
+                    {
+                        for (int l = 0; l < GLobalJadwal.GlobalSolusi[k].Solusi[i].Mengajar.Count; l++)
+                        {
+                            if (GLobalJadwal.GlobalSolusi[pos].Solusi[i].Mengajar[j].Guru == GLobalJadwal.GlobalSolusi[k].Solusi[i].Mengajar[l].Guru)
+                            {
+                                if (Math.Abs(GLobalJadwal.GlobalSolusi[pos].Solusi[i].Mengajar[j].StartMengajar - GLobalJadwal.GlobalSolusi[k].Solusi[i].Mengajar[l].StartMengajar) <= GLobalJadwal.GlobalSolusi[pos].Solusi[i].Mengajar[j].Sks)
+                                {
+                                    pinalti++;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+                return pinalti;
+        }
+
+        public List<int> insertion(solusi jadwal)
+        {
+            int random = 0;
+            List<int> x = new List<int>();
+            for(int i = 0; i < jadwal.Solusi.Count){
+                    random = rr.Next(0, jadwal.Solusi[i].Mengajar.Count-1);
+                x.Add(random);
+            }
+            return x;
+        }
+
+        public List<solusi> intensification(solusi jadwal)
+        {
+            List<solusi> x;
+            List<solusi> temp;
+            solusi temp_jadwal = jadwal;
+            List<int> job = insertion(jadwal);
+            for (int i = 0; i < temp_jadwal.Solusi.Count; i++)
+            {
+               //intensification macet
+            }
+                return x;
         }
     }
 }
